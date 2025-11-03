5353ad2b0d7508b41929765dcc50bf973b5e289a
KoLincoLopez <lincopilconstanza3@gmail.com>
2025-11-03 14:25:59 -0300

    Revert "update"
    This reverts commit 29d51857fd4eac8329053ec85f74b9eec980bf69.

--- rutas-secretas-react/src/App.jsx
+++ rutas-secretas-react/src/App.jsx
@@ -1,28 +1,35 @@
-// src/App.jsx
-
-import React from 'react';
-// Importamos el CSS de Bootstrap (instalado vía npm)
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-
-// Importamos tus estilos CSS
-import './styles/Global.css'; 
-import './styles/Index.css'; 
-
-import Navbar from './components/Navbar'; 
-import Footer from './components/Footer'; 
-import Home from './pages/Home'; // El contenido principal de tu Index.html
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  // Nota: En una aplicación con múltiples páginas, aquí usarías React Router 
-  // para cambiar el componente entre <Home />, <Productos />, etc.
-  
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <Navbar />
-      <Home /> {/* Esto contiene todo el <main> */}
-      <Footer />
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
